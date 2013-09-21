9d6c116e0806e024d3e7bd4be9b3a3e4555c3b24
nmost <nick@mostowich.com>
2013-09-21 09:22:44 -0700

    fucking heroku

--- app.js
+++ app.js
@@ -118,6 +118,6 @@ app.get('/route/:startLoc/:endLoc', function(req, res){
   }, false);
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
